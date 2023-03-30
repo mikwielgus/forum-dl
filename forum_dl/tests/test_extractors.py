353d77966b6286ea04a172c80b46a80afa4a9630
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2023-03-30 20:16:06 +0200

    tests/extractors: Split each per-extractor testcase into separate pytest test

--- forum_dl/tests/test_extractors.py
+++ forum_dl/tests/test_extractors.py
@@ -10,79 +10,83 @@ import itertools
 import hashlib
 import pytest
 
+testdata = []
+for cls in extractors.list_classes():
+    for test in cls.tests:
+        testdata.append((cls, test))
 
-@pytest.mark.parametrize("cls", extractors.list_classes())
-def test_extractors(cls: Type[ForumExtractor]):
+
+@pytest.mark.parametrize("cls,test", testdata)
+def test_extractors(cls: Type[ForumExtractor], test: dict[str, Any]):
     session = CachedSession()
 
-    for test in cls.tests:
-        url = test.pop("url")
-        print(f"url: {url}")
+    url = test.pop("url")
+    print(f"url: {url}")
 
-        extractor = cls.detect(session, url)
+    extractor = cls.detect(session, url)
 
-        assert isinstance(extractor, cls)
+    assert isinstance(extractor, cls)
 
-        if test_base_url := test.pop("test_base_url", None):
-            assert extractor._base_url == test_base_url
+    if test_base_url := test.pop("test_base_url", None):
+        assert extractor._base_url == test_base_url
 
-        extractor.fetch()
+    extractor.fetch()
 
-        base_node = extractor.node_from_url(url)
+    base_node = extractor.node_from_url(url)
 
-        if test_boards := test.pop("test_boards", None):
-            assert isinstance(base_node, Board)
+    if test_boards := test.pop("test_boards", None):
+        assert isinstance(base_node, Board)
 
-            boards = list(
-                itertools.islice(
-                    extractor.subboards(base_node), test.pop("board_count", None)
-                )
+        boards = list(
+            itertools.islice(
+                extractor.subboards(base_node), test.pop("board_count", None)
             )
-            print(f"boards: {boards}")
-
-            for path, test_board in test_boards.items():
-                board = extractor.find_board(path)
-                assert list(path) == board.path
-
-                if test_title := test_board.pop("title"):
-                    assert board.title == test_title
-
-                if test_content := test_board.pop("content", None):
-                    assert board.content == test_content
-
-                assert not test_board
-
-        items = list(
-            itertools.islice(extractor.items(base_node), test.pop("item_count", None))
         )
-        print(f"items: {items}")
+        print(f"boards: {boards}")
 
-        if test_items := test.pop("test_items", None):
-            for i, item in enumerate(items):
-                if test_item := test_items.pop(i, None):
-                    print(i, test_item)
+        for path, test_board in test_boards.items():
+            board = extractor.find_board(path)
+            assert list(path) == board.path
 
-                    if test_title := test_item.pop("title"):
-                        assert item.title == test_title
+            if test_title := test_board.pop("title"):
+                assert board.title == test_title
 
-                    if test_path := test_item.pop("path"):
-                        assert item.path == test_path
+            if test_content := test_board.pop("content", None):
+                assert board.content == test_content
 
-                    assert not test_item
+            assert not test_board
 
-            assert not test_items
+    items = list(
+        itertools.islice(extractor.items(base_node), test.pop("item_count", None))
+    )
+    print(f"items: {items}")
 
-        if test_item_count := test.pop("test_item_count", None):
-            assert len(items) == test_item_count
+    if test_items := test.pop("test_items", None):
+        for i, item in enumerate(items):
+            if test_item := test_items.pop(i, None):
+                print(i, test_item)
 
-        if test_min_item_count := test.pop("test_min_item_count", None):
-            assert len(items) >= test_min_item_count
+                if test_title := test_item.pop("title"):
+                    assert item.title == test_title
 
-        contents = [item.content for item in items]
-        hash = hashlib.sha1("\0".join(contents).encode("utf-8")).hexdigest()
-        print(f"hash: {hash}")
+                if test_path := test_item.pop("path"):
+                    assert item.path == test_path
 
-        if test_contents_hash := test.pop("test_contents_hash", None):
-            assert hash == test_contents_hash
+                assert not test_item
 
-        assert not test
+        assert not test_items
+
+    if test_item_count := test.pop("test_item_count", None):
+        assert len(items) == test_item_count
+
+    if test_min_item_count := test.pop("test_min_item_count", None):
+        assert len(items) >= test_min_item_count
+
+    contents = [item.content for item in items]
+    hash = hashlib.sha1("\0".join(contents).encode("utf-8")).hexdigest()
+    print(f"hash: {hash}")
+
+    if test_contents_hash := test.pop("test_contents_hash", None):
+        assert hash == test_contents_hash
+
+    assert not test
