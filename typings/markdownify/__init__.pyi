dd061df8444c3a16bc6003693b9810174fa2bcf9
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2023-04-05 02:32:48 +0200

    cleanup: Fix formatting of Markdownify stub

--- typings/markdownify/__init__.pyi
+++ typings/markdownify/__init__.pyi
@@ -1,6 +1,5 @@
 # pyright: strict
 from __future__ import annotations
-from typing import * # type: ignore
+from typing import *  # type: ignore
 
-def markdownify(html: str, **options: Any) -> str:
-    ...
+def markdownify(html: str, **options: Any) -> str: ...
